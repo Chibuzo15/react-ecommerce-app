0e6d1e7ee2955f79f0eeac13e0a3523ceafde990
Chibuzo15 <Anyachris85@gmail.com>
2020-05-16 14:03:52 +0100

    Heroku config

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.use((err, req, res, next) => {
 app.use('/api', routes);
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static(path.join(__dirname, '../client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'))
